31ff419dfbde6e551bf1ce84dd90ce2e1559a775
114lohith-sudo <114lohith@gmail.com>
2026-09-05 10:25:19 -0500

    Snowman project

---
+++ snowman.py
@@ -0,0 +1,18 @@
+import pgzrun
+
+WIDTH=450
+HEIGHT=500
+
+def draw():
+    screen.draw.filled_circle((225,420),75,"white")
+    screen.draw.filled_circle((225,295),55,"white")
+    screen.draw.filled_circle((225,200),40,"white")
+
+    screen.draw.filled_circle((240,190),5,"black")
+    screen.draw.filled_circle((210,190),5,"black")
+
+
+
+
+
+pgzrun.go()
