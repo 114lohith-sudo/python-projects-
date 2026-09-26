ff4f724b6a262b716e126526f9e58fcec8e553c9
114lohith-sudo <114lohith@gmail.com>
2026-09-26 10:55:18 -0500

    set notes

---
+++ sets.py
@@ -0,0 +1,11 @@
+#set removes duplicates when run
+set1={1,2,3,4,5,5}
+print(set1)
+#adds a number to a set
+set1.add(6)
+print(set1)
+#removes a number from a set
+set1.remove(1)
+print(set1)
+#discard is used when are not sure of the exsistance of an element
+set1.discard(7)
